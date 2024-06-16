70ddd366bc69f41b24b132aff69263220612eae4
winternotseason <winternotseason@naver.com>
2024-06-16 22:47:38 +0900

    Detail meal page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,10 +1,39 @@
-import React from "react";
+import { getMeal } from "@/lib/meals";
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import classes from "./page.module.css";
 
-const DetailMealPage = ({ params }) => {
+const DetailMealPage = async ({ params }) => {
+  const meal = await getMeal(params.slug);
+
+  if (!meal) {
+    notFound();
+  }
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br .>");
   return (
-    <div>
-      <h1>DetailMealPage about {params.slug}</h1>
-    </div>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} fill alt={meal.slug} />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main className={classes.main}>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions,
+          }}
+        ></p>
+      </main>
+    </>
   );
 };
 
