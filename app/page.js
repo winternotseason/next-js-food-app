6ec19ae53740d9adf7bf6ea28e2a4bf5d8312d2e
winternotseason <winternotseason@naver.com>
2024-06-16 13:32:21 +0900

    image slideshow : client component

--- app/page.js
+++ app/page.js
@@ -1,3 +1,4 @@
+import ImageSlideshow from "@/components/images/image-slideshow";
 import Link from "next/link";
 import classes from "./page.module.css";
 
@@ -5,7 +6,9 @@ export default function Home() {
   return (
     <>
       <header className={classes.header}>
-        <div className={classes.slideshow}></div>
+        <div className={classes.slideshow}>
+          <ImageSlideshow />
+        </div>
         <div>
           <div className={classes.hero}>
             <h1>NextLevel Food for NextLevel Foodies</h1>
