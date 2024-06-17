256b79511d1df37628f766bb6d2d2cafb8e69d43
winternotseason <winternotseason@naver.com>
2024-06-17 15:06:48 +0900

    saving form data in DB

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,23 +1,9 @@
 import ImagePicker from "@/components/meals/image-picker";
+import { shareMeal } from "@/lib/action";
 import classes from "./page.module.css";
 
 export default function ShareMealPage() {
-  /* 서버 액션 함수 */
-  const shareMeal = async (formData) => {
-    /* 함수 안에서의 use server : 오직 서버에서만 실행될 수 있게 보장 */
-    "use server";
-    /* 제출된 데이터 다루기 */
-    const meal = {
-      /* get(input 필드의 name) */
-      title: formData.get("title"),
-      summary: formData.get("summary"),
-      instructions: formData.get("instructions"),
-      image: formData.get("image") /* file 일 것 */,
-      creator: formData.get("name"),
-      creator_email: formData.get("email"),
-    };
-    console.log(meal);
-  };
+ 
   return (
     <>
       <header className={classes.header}>
